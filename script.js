b64d79ba812c9476b7b66e66c5748af2ca1c6c30
Claenio <claenio.souza@gmail.com>
2021-10-11 21:05:01 -0300

    changed

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function alerta(){
-    console.log("helou")
+var clok = setInterval(clock, 1000)
+function clock(){
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    document.getElementById("idclock").innerHTML = h+":"+m+":"+s
+    
+    if (h >= 6 && h <= 11){
+        document.getElementById("msg").innerHTML = "Good morning! It's "
+    }else if (h >= 12 && h <=17){
+        document.getElementById("msg").innerHTML = "Good aftetnoon! It's "
+    }else if (h >= 18 && h <= 23){
+        document.getElementById("msg").innerHTML = "Good evening! It's "
+    }else if (h >= 0 && h <= 5){
+        document.getElementById("msg").innerHTML = "Sleep time!! Now it's "
+    }
 }
-alerta()
+clock()
+
+
+
+
+
+
